355362776a6e9a883277e1812db376b318cd56c4
jinellebaterna <jinelle@mrkdwn.io>
2020-03-10 13:13:05 -0400

    added public resume link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./App.module.scss";
-import { Layout, Header, Navigation, Content, Drawer } from "react-mdl";
-import { Link } from "react-router-dom";
+import {  Content } from "react-mdl";
 import Main from "./Components/Main";
 
 class App extends React.Component {
@@ -9,25 +8,24 @@ class App extends React.Component {
     return (
       <div className={styles.app}>
         <div>
-          <Layout>
-            {/*<Header className={styles.header} title=" ">*/}
-            {/*  <Navigation>*/}
-            {/*    <Link to="/"> Resume</Link>*/}
-            {/*    <Link to="/projects"> Projects</Link>*/}
-            {/*    <Link to="/contact"> Contact</Link>*/}
-            {/*  </Navigation>*/}
-            {/*</Header>*/}
-            {/*<Drawer title="">*/}
-            {/*  <Navigation>*/}
-            {/*    <Link to="/"> Resume</Link>*/}
-            {/*    <Link to="/projects"> Projects</Link>*/}
-            {/*    <Link to="/contact"> Contact</Link>*/}
-            {/*  </Navigation>*/}
-            {/*</Drawer>*/}
+            {/*<Layout>*/}
+            {/*  <Header className={styles.header} title=" ">*/}
+            {/*    <Navigation>*/}
+            {/*      <Link to="/"> Resume</Link>*/}
+            {/*      <Link to="/projects"> Projects</Link>*/}
+            {/*    </Navigation>*/}
+            {/*  </Header>*/}
+            {/*  <Drawer title="">*/}
+            {/*    <Navigation>*/}
+            {/*      <div className={styles.container}/>*/}
+            {/*      <Link to="/"> Resume</Link>*/}
+            {/*      <Link to="/projects"> Projects</Link>*/}
+            {/*    </Navigation>*/}
+            {/*  </Drawer>*/}
             <Content>
               <Main />
             </Content>
-          </Layout>
+          {/*</Layout>*/}
         </div>
       </div>
     );
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,14 +4,40 @@ import styles from "./Projects.module.scss";
 class Projects extends React.Component {
   render() {
     return (
-      <div className={styles.projects}>
-        <h1> Sample Projects here </h1>
-        <h2> Mrkdwn.ph </h2>
-        <p>
-          Built for everyone who wants to spend less, Mrkdwn.ph is a platform
-          for discovering all the latest deals, coupons, and discounts from your
-          favorite brands and stores.
-        </p>
+      <div className={styles.container}>
+        <ul>
+          <li>
+            <a
+              href="https://www.mrkdwn.ph/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Mrkdwn.ph
+            </a>
+          </li>
+          <li>
+            Built for everyone who wants to spend less, Mrkdwn is a platform for
+            discovering all the latest deals, coupons, and discounts from your
+            favorite brands and stores. Mrkdwn also helps merchants reach and
+            understand their potential customers, and increase brand exposure,
+            and revenue.
+          </li>
+          <hr/>
+          <li>
+            <a
+              href="https://dev.wfhjobs.ph/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              WFHJobs
+            </a>
+          </li>
+          <li>
+            WFHJobs is a platform built for individuals pursuing remote work.
+            WFHJobs help companies reach and find talents by posting their
+            job on our site.
+          </li>
+        </ul>
       </div>
     );
   }
