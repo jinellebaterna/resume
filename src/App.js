f5fbfd40fb929a7d7e98cf1ea1b55dae5b8a7f43
jinellebaterna <jinelle@mrkdwn.io>
2020-08-06 17:10:41 -0400

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import styles from "./App.module.scss";
-import { Content, Layout } from "react-mdl";
 import Main from "./Components/Main/Main";
-import Navigation from "./Components/Navigation/Navigation";
 import Header from "./Components/Header/Header";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Projects from "./Components/Projects/Projects";
+import Resume from "./Components/Resume/Resume";
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.app}>
-          <Layout>
-            <Header/>
-              <Navigation />
-            <Content>
-              <Main/>
-            </Content>
-          </Layout>
+       <BrowserRouter>
+         <Header/>
+         <div className={styles.content}>
+           <Route exact path="/" component={Home} />
+           <Route path="/projects" component={Projects} />
+           <Route path="/resume" component={Resume} />
+         </div>
+       </BrowserRouter>
       </div>
     );
   }
